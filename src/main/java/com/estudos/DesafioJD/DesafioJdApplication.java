1f157af4fb6c76a793aa995efc202627e031e1e9
Renan Moura <renanmoour@gmail.com>
2026-01-02 22:34:05 -0300

    Primeiro commit

---
+++ src/main/java/com/estudos/DesafioJD/DesafioJdApplication.java
@@ -0,0 +1,13 @@
+package com.estudos.DesafioJD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioJdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioJdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudos/DesafioJD/DesafioJdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.DesafioJD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioJdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
